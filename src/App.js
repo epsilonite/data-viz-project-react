c8cb1a43295f479800d686c301e17fd4938d124c
be <be.han.eps@gmail.com>
2024-09-18 11:09:40 -0700

    changed approach

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
+import React from 'react';
 import './App.css';
-import { Routes, Route} from "react-router-dom";
-import About from "./routes/About";
-import Data from "./routes/Data";
-import Home from "./routes/Home";
-import Navbar from './Navbar';
-function App() {
-  return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/data" element={<Data />} />
-      </Routes>
-    </>
+import Map from './components/Map.js';
+import Legend from './components/Legend';
+const App = () => {
+   return(
+     <div className='container'>
+       <Map/>
+       <Legend/>
+     </div>
   );
-}
+};
 export default App;
